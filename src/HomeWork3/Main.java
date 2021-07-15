9182d435fe65219a245e7c1916b3180731edda6c
JoJo <svobiv@ya.ru>
2021-07-15 22:32:09 +0600

    hw_3

---
+++ src/HomeWork3/Main.java
@@ -0,0 +1,17 @@
+package HomeWork3;
+
+import java.util.Scanner;
+
+public class Main {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+//        int x = scanner.nextInt();
+//        int y = scanner.nextInt();
+//        System.out.println(x + "+" + y + " = " + (x+y));
+//        System.out.printf("%d + %d = %d\n ",x , y ,x+y);
+
+//        String name = scanner.nextLine();
+//        System.out.println("Hello" +name);
+    }
+}
